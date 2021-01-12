088cdd4c8a7bbc28f4f76947fee77405f5df6354
Emily <>
2021-01-12 13:07:47 -0500

    length

--- Radix.java
+++ Radix.java
@@ -2,4 +2,14 @@ public class Radix{
   public static int nth(int n, int col){
     return (int) (n % Math.pow(10, (col+1)) / Math.pow(10, col));
   }
+
+  public static int length(int n){
+    int count = 0;
+    while(n != 0){
+      n = n/10;
+      count ++;
+    }
+    return count;
+  }
+
 }
