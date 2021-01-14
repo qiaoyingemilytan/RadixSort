9bb9ea412a9e25ed831ba9257ce36879e4d42f8f
Emily <>
2021-01-14 03:09:27 -0500

    simple sort

--- Radix.java
+++ Radix.java
@@ -18,4 +18,25 @@ public class Radix{
     }
   }
 
+  public static void radixSortSimple(SortableLinkedList data){
+    int maxPlace = 0;
+    for(int j = 0; j < data.size(); j++){
+      if(length(data.get(j)) > maxPlace){
+        maxPlace = length(data.get(j));
+      }
+    }
+    for(int places = 0; places <= maxPlace; maxPlace++){
+      SortableLinkedList[] buckets = new SortableLinkedList[10];
+      SortableLinkedList pass = new SortableLinkedList();
+      for(int i = 0; i < 10; i++){
+        buckets[i] = new SortableLinkedList();
+      }
+      for(int key = 0; key < data.size(); key++){
+        buckets[nth(data.get(key), places)].add(data.get(key));
+      }
+      merge(pass, buckets);
+      data = pass;
+    }
+  }
+
 }
